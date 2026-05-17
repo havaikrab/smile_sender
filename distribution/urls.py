6f34826186e9bf9fe64dd94084ddf30c66ce8526
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-05-17 20:37:17 +0700

    made init migrations, check runserver

--- distribution/urls.py
+++ distribution/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = DistributionConfig.name
 
 urlpatterns = [
-    path('', views.TemplateView.as_view(), name='main'),
+    path('', views.MainView.as_view(), name='main'),
 ]
