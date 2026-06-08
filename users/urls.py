bd644af47debf1823ea303566b60044816c20f15
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-06-08 09:25:47 +0700

    add logout view

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
@@ -11,4 +11,5 @@ urlpatterns: list = [
     path("register/", views.CustomUserRegisterView.as_view(), name="register"),
     path("activate/<int:pk>/<token>/", views.CustomUserActivationView.as_view(), name="activate"),
     path("login/", LoginView.as_view(template_name="users/login.html", form_class=CustomUserLoginForm), name="login"),
+    path("logout/", LogoutView.as_view(next_page="distribution:main"), name="logout"),
 ]
